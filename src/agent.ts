3b3b62e448326fa35930bae271ac83a00bef964f
0xtrademarc <marcusloo1338@gmail.com>
2022-07-28 00:10:11 +0800

    refac: moved constants to utils.ts

--- src/agent.ts
+++ src/agent.ts
@@ -9,6 +9,11 @@ import {
   getJsonRpcUrl,
 } from "forta-agent";
 
+import {
+  UniV3FactoryABI,
+  UniV3PoolABI,
+} from "./utils"
+
 const ethers = require('ethers');
 
 // add these variables to a separate data file
@@ -16,21 +21,16 @@ export const SWAP_EVENT =
   "event Swap(address indexed sender,address indexed recipient, int256 amount0,int256 amount1,uint160 sqrtPriceX96, uint128 liquidity, int24 tick)"
 export const UNISWAPV3FACTORY_ADDRESS = "0x1f98431c8ad98523631ae4a59f267346ea31f984"
 
-// connecting to the contract
-// The Contract ABI - THIS IS BAD, MOVE TO DATA FILE
-let UniV3FactoryABI = [{ "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "uint24", "name": "fee", "type": "uint24" }, { "indexed": true, "internalType": "int24", "name": "tickSpacing", "type": "int24" }], "name": "FeeAmountEnabled", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "oldOwner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }], "name": "OwnerChanged", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "token0", "type": "address" }, { "indexed": true, "internalType": "address", "name": "token1", "type": "address" }, { "indexed": true, "internalType": "uint24", "name": "fee", "type": "uint24" }, { "indexed": false, "internalType": "int24", "name": "tickSpacing", "type": "int24" }, { "indexed": false, "internalType": "address", "name": "pool", "type": "address" }], "name": "PoolCreated", "type": "event" }, { "inputs": [{ "internalType": "address", "name": "tokenA", "type": "address" }, { "internalType": "address", "name": "tokenB", "type": "address" }, { "internalType": "uint24", "name": "fee", "type": "uint24" }], "name": "createPool", "outputs": [{ "internalType": "address", "name": "pool", "type": "address" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint24", "name": "fee", "type": "uint24" }, { "internalType": "int24", "name": "tickSpacing", "type": "int24" }], "name": "enableFeeAmount", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint24", "name": "", "type": "uint24" }], "name": "feeAmountTickSpacing", "outputs": [{ "internalType": "int24", "name": "", "type": "int24" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "", "type": "address" }, { "internalType": "address", "name": "", "type": "address" }, { "internalType": "uint24", "name": "", "type": "uint24" }], "name": "getPool", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "parameters", "outputs": [{ "internalType": "address", "name": "factory", "type": "address" }, { "internalType": "address", "name": "token0", "type": "address" }, { "internalType": "address", "name": "token1", "type": "address" }, { "internalType": "uint24", "name": "fee", "type": "uint24" }, { "internalType": "int24", "name": "tickSpacing", "type": "int24" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "_owner", "type": "address" }], "name": "setOwner", "outputs": [], "stateMutability": "nonpayable", "type": "function" }]
-//let provider = ethers.getDefaultProvider();
+//let provider = ethers.getDefaultProvider(); - whats the diff between this and the line below?
 let provider = new ethers.providers.JsonRpcProvider([getJsonRpcUrl()])
 let UniswapV3FactoryContract = new ethers.Contract(UNISWAPV3FACTORY_ADDRESS,
   UniV3FactoryABI, provider);
 
-
-let UniV3PoolABI = [{ "inputs": [], "name": "fee", "outputs": [{ "internalType": "uint24", "name": "", "type": "uint24" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "protocolFees", "outputs": [{ "internalType": "uint128", "name": "token0", "type": "uint128" }, { "internalType": "uint128", "name": "token1", "type": "uint128" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "token0", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }]
 // function to check if a poolAddress is a UniswapV3 pool
 // logic should take advantage of getPool(), which returns the pools address
 // when a swap event is emitted in a transaction.
 async function isUniswapV3Pool(poolAddress: string) {
-  let UniswapV3PoolContract = new ethers.Contract(poolAddress, UniV3FactoryABI, provider);
+  let UniswapV3PoolContract = new ethers.Contract(poolAddress, UniV3PoolABI, provider);
   const tokenA = await UniswapV3PoolContract.token0();
   const tokenB = await UniswapV3PoolContract.token1();
   const fee = await UniswapV3PoolContract.fee();
